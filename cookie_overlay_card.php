ee411607908d07cef78d3ee375947fe36785586a
shahzaibriaz69 <shahzaibraiz0023@gmail.com>
2026-03-17 14:09:17 +0500

    creates file of all cards now its complete

---
+++ cookie_overlay_card.php
@@ -0,0 +1,70 @@
+<?php include 'header.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Fursgo</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"
+        rel="stylesheet">
+</head>
+
+<body>
+    <section class="cookie-overlay">
+
+        <div class="fs-cookie-modal">
+
+
+            <div class="fs-cookie-header">
+                <span>Privacy Preference Center</span>
+                <span class="fs-close-btn"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                        viewBox="0 0 20 20" fill="none">
+                        <circle cx="10" cy="10" r="9.5" stroke="white" />
+                        <path d="M7.11133 13.3331L13.3336 7.11084M7.11133 7.11084L13.3336 13.3331" stroke="white"
+                            stroke-width="1.5" stroke-linecap="round" />
+                    </svg></span>
+            </div>
+
+
+            <div class="fs-cookie-content">
+
+                <p>
+                    We use cookies to make our site work and improve your experience.
+                    You can manage your cookie preferences at any time.
+                </p>
+
+                <div class="fs-cookie-buttons">
+
+                    <button class="fs-allow-btn">Allow All</button>
+                    <button class="fs-decline-btn">Decline unnecessary cookies</button>
+
+                </div>
+
+                <button class="fs-manage-btn">
+                    <p>Manage consent preferences</p>
+                </button>
+
+                <div class="fs-learn-more">
+                    <a href="#">Learn More</a>
+                </div>
+
+
+
+            </div>
+
+        </div>
+
+    </section>
+
+</body>
+
+</html>
